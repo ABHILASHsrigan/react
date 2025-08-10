0e7a1a8f107eb263672b7a4d48baf8246fcc293a
ABHILASH SRIGAN <sriganabhilash@gmail.com>
2025-08-10 10:32:03 +0530

    Update Todos.jsx

--- todo redux/redux-todo/src/Components/Todos.jsx
+++ todo redux/redux-todo/src/Components/Todos.jsx
@@ -1,38 +1,80 @@
 import React, { useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
-import { add , remove ,update} from '../features/todoSlice';
+import { add, remove, update } from '../features/todoSlice';
 
 function TodoComponent() {
     const [valueAdd, setvalueAdd] = useState('');
+    const [editIndex, setEditIndex] = useState(null);
     const dispatch = useDispatch();
     const todos = useSelector((state) => state.todos.value);
 
-    const handleAddTodo = () => {
+    const handleAddOrUpdateTodo = () => {
         if (valueAdd.trim()) {
-            dispatch(add(valueAdd));
+            if (editIndex !== null) {
+                dispatch(update({ index: editIndex, title: valueAdd }));
+                setEditIndex(null);
+            } else {
+                dispatch(add(valueAdd));
+            }
             setvalueAdd('');
         }
     };
 
+    const handleEdit = (i) => {
+        setvalueAdd(todos[i].title);
+        setEditIndex(i);
+    };
+
     return (
         <div>
-            <h2>Todos:</h2>
+            <h2 style={{ textAlign: 'center', margin: '20px 0' }}>TODO APP </h2>
 
             <div>
                 <input
+                    style={{
+                        padding: '10px',
+                        width: '300px',
+                        borderRadius: '5px',
+                        border: '1px solid #ccc',
+                        position: 'absolute',
+                        left: '500px',
+                        top: '200px'
+                    }}
                     type="text"
                     value={valueAdd}
                     onChange={(e) => setvalueAdd(e.target.value)}
                 />
-                <button onClick={handleAddTodo}>Add Todo</button>
+                <button
+                    onClick={handleAddOrUpdateTodo}
+                    style={{
+                        padding: '10px 15px',
+                        borderRadius: '5px',
+                        border: 'none',
+                        background: editIndex !== null ? '#ffc107' : '#28a745',
+                        color: '#fff',
+                        cursor: 'pointer',
+                        position: 'absolute',
+                        left: '850px',
+                        top: '200px'
+                    }}
+                >
+                    {editIndex !== null ? 'Update Todo' : 'Add Todo'}
+                </button>
             </div>
 
             <ul>
                 {todos.map((item, i) => (
-                    <li key={i}>
+                    <li
+                        style={{
+                            position: 'absolute',
+                            left: '550px',
+                            top: `${300 + i * 40}px`
+                        }}
+                        key={i}
+                    >
                         {item.title} - {item.status ? 'Completed' : 'In Completed'}
-                        <button onClick={()=>dispatch(update(i))}>Edit</button>
-                        <button onClick={()=>dispatch(remove(i))}>Delete</button>
+                        <button onClick={() => handleEdit(i)} style={{ marginLeft: '10px' }}>Edit</button>
+                        <button onClick={() => dispatch(remove(i))} style={{ marginLeft: '5px' }}>Delete</button>
                     </li>
                 ))}
             </ul>
