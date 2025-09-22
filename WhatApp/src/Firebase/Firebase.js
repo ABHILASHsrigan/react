b0aaaf530a0fc9eed15d981dc4a683d83f3cc99a
ABHILASH SRIGAN <sriganabhilash@gmail.com>
2025-09-22 11:13:40 +0530

    Update Firebase.js

--- WhatApp/src/Firebase/Firebase.js
+++ WhatApp/src/Firebase/Firebase.js
@@ -1,22 +1,21 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
-
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyABu076TbRIezFDGF3G662XRG5ruaF__Hg",
-  authDomain: "react-firebase-a5c2f.firebaseapp.com",
-  projectId: "react-firebase-a5c2f",
-  storageBucket: "react-firebase-a5c2f.firebasestorage.app",
-  messagingSenderId: "87743648844",
-  appId: "1:87743648844:web:104278c7f4a5aeea0650e9",
-  measurementId: "G-DJTGHJ3CXQ"
+  apiKey: "AIzaSyBejipK4AlPRVf3uJwVGd_BfvlM7_xVRhY",
+  authDomain: "chat-app-422e3.firebaseapp.com",
+  projectId: "chat-app-422e3",
+  storageBucket: "chat-app-422e3.firebasestorage.app",
+  messagingSenderId: "742207036754",
+  appId: "1:742207036754:web:59d8f59ddd8829e4e06e8b",
+  measurementId: "G-NVKPMNQPZ5"
 };
 
 // Initialize Firebase
-export const app = initializeApp(firebaseConfig);
-const chats = getAnalytics(app);
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
